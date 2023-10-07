e3f725c1ce47ac0a3ba2f06298d69591bce97fa9
Swapna2005 <146418921+Swapna2005@users.noreply.github.com>
2023-10-07 09:30:43 +0530

    Create super functions.py

---
+++ super functions.py
@@ -0,0 +1,15 @@
+# class Emp():
+
+#     def __init__(self,id,name,add):
+#         self.id=id
+#         self.name=name
+#         self.add=add
+# class freelance(Emp):
+#     def __init__(self,id,name,add,emails):
+#         super().__init__(id,name,add)
+#         self.emails=emails
+# emp_1=freelance (103,"swapna","palakkad","swapna@gmail")
+# print('the id is:',emp_1.id)
+# print('the name is:',emp_1.name)
+# print('the add is:',emp_1.add)
+# print('the emails is:',emp_1.add)
